71162e56e6e8ec82c45f1b3957747821c3d2c902
llong2195 <nduylong9501@gmail.com>
2026-02-10 17:03:22 +0700

    feat(graphql): add index file to export fragment-masking and gql modules

--- codegen.ts
+++ codegen.ts
@@ -1,10 +1,41 @@
 import type { CodegenConfig } from "@graphql-codegen/cli";
 
+// const config: CodegenConfig = {
+//   overwrite: true,
+//   schema: "https://shopify.dev/admin-graphql-direct-proxy/2026-01",
+//   documents: ["src/**/*.ts"],
+//   ignoreNoDocuments: true,
+//   generates: {
+//     "src/generated/graphql.ts": {
+//       plugins: ["typescript"],
+//     },
+//     "src/types": {
+//       preset: "client",
+//       config: {
+//         documentMode: "string",
+//       },
+//     },
+//   },
+// };
+
 const config: CodegenConfig = {
-  overwrite: true,
   schema: "https://shopify.dev/admin-graphql-direct-proxy/2026-01",
+  documents: ["src/**/*.ts"],
+  ignoreNoDocuments: true,
   generates: {
-    "src/generated/graphql.ts": {
+    "./src/graphql/": {
+      preset: "client",
+      config: {
+        documentMode: "string",
+      },
+    },
+    "./schema.graphql": {
+      plugins: ["schema-ast"],
+      config: {
+        includeDirectives: true,
+      },
+    },
+    "./src/generated/graphql.ts": {
       plugins: ["typescript"],
     },
   },
